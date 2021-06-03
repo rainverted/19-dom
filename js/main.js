37e34fa06c982f4004a916788e4972dc4ad2d3ba
rainverted <MY_NAME@example.com>
2021-06-03 20:31:26 +0300

    h1 resetinimas pasirinkus funkcija

--- js/main.js
+++ js/main.js
@@ -3,18 +3,23 @@ let rezultatas = 0;
 const minusDOM = document.querySelector('.minus');
 const plusDOM = document.querySelector('.plus');
 const numberDOM = document.querySelector('.number');
+const h1DOM = document.querySelector('h1')
 
 minusDOM.addEventListener('click', () => {
     numberDOM.innerText = --rezultatas;
+    h1DOM.innerText = 'Žaidimas progrese';
 })
 
 plusDOM.addEventListener('click', () => {
     numberDOM.innerText = ++rezultatas;
+    h1DOM.innerText = 'Žaidimas progrese';
 })
+
 
 numberDOM.addEventListener('click', () => {
     rezultatas = 0;
     numberDOM.innerText = rezultatas;
+    h1DOM.innerText = 'Te prasideda zaidynes!🥳';
 })
 
 
