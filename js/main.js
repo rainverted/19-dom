53db9049130f275797cfe058c6a5e8a2509faf01
rainverted <MY_NAME@example.com>
2021-06-04 19:46:35 +0300

    zinuciu sarasas

--- js/main.js
+++ js/main.js
@@ -16,17 +16,20 @@ ulDOM.innerHTML = `<li>Tu pasileidai zaidima - sekmes!</li>`;
 minusDOM.addEventListener('click', () => {
     numberDOM.innerText = --rezultatas;
     h1DOM.innerText = gameInProgressMessage;
+    ulDOM.innerHTML += `<li>Paspaudei minusa ir dabartinis rezultatas yra ${numberDOM.innerText}.</li>`;
 })
 
 plusDOM.addEventListener('click', () => {
     numberDOM.innerText = ++rezultatas;
     h1DOM.innerText = gameInProgressMessage;
+    ulDOM.innerHTML += `<li>Paspaudei pliusa ir dabartinis rezultatas yra ${numberDOM.innerText}.</li>`;
 })
 
 resetDOM.addEventListener('click', () => {
     rezultatas = 0;
     numberDOM.innerText = rezultatas;
     h1DOM.innerText = initialH1Text;
+    ulDOM.innerHTML += `<li>Paspaudei reset ir dabartinis rezultatas yra ${numberDOM.innerText}.</li>`;
 })
 
 numberDOM.addEventListener('click', () => {
