a79f09dd0cf4673691959accd1ee2b13efc21578
rainverted <MY_NAME@example.com>
2021-06-05 09:57:17 +0300

    Uzduoties Ivestis

--- js/main.js
+++ js/main.js
@@ -1,44 +1,10 @@
-let rezultatas = 0;
-
-const minusDOM = document.querySelector('.minus');
-const plusDOM = document.querySelector('.plus');
-const numberDOM = document.querySelector('.number');
-const resetDOM = document.querySelector('.reset');
-const h1DOM = document.querySelector('h1');
-const ulDOM = document.querySelector('ul');
-
-const gameInProgressMessage = 'Žaidimas progrese';
-const initialH1Text = h1DOM.innerText;
-
-ulDOM.innerHTML = `<li>Tu pasileidai zaidima - sekmes!</li>`;
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const span = document.querySelector('span');
 
 
-minusDOM.addEventListener('click', () => {
-    numberDOM.innerText = --rezultatas;
-    h1DOM.innerText = gameInProgressMessage;
-    ulDOM.innerHTML += `<li>Paspaudei minusa ir dabartinis rezultatas yra ${numberDOM.innerText}.</li>`;
+buttonDOM.addEventListener('click', (event) => {
+    event.preventDefault(); // neleidzia puslapiui persikrauti
+    spanDOM.innerText = inputDOM.value;
+    inputDOM.value = '';
 })
-
-plusDOM.addEventListener('click', () => {
-    numberDOM.innerText = ++rezultatas;
-    h1DOM.innerText = gameInProgressMessage;
-    ulDOM.innerHTML += `<li>Paspaudei pliusa ir dabartinis rezultatas yra ${numberDOM.innerText}.</li>`;
-})
-
-resetDOM.addEventListener('click', () => {
-    rezultatas = 0;
-    numberDOM.innerText = rezultatas;
-    h1DOM.innerText = initialH1Text;
-    ulDOM.innerHTML += `<li>Paspaudei reset ir dabartinis rezultatas yra ${rezultatas}.</li>`;
-})
-
-numberDOM.addEventListener('click', () => {
-    rezultatas = 0;
-    numberDOM.innerText = rezultatas;
-    h1DOM.innerText = 'Te prasideda zaidynes!🥳';
-})
-
-
-
-
-
