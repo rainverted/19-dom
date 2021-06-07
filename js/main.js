78e1fe1835e0dade4d9d90c71a8669fc993b8753
rainverted <MY_NAME@example.com>
2021-06-07 18:28:47 +0300

    uzsakymo spausdinimas

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
-const input = document.querySelector('input');
-const button = document.querySelector('button');
-const span = document.querySelector('span');
+const customerInputDOM = document.querySelector('#customer');
+const sriubaDOM = document.querySelector('#sriuba');
+const patiekalasDOM = document.querySelector('#patiekalas');
+const desertasDOM = document.querySelector('#desertas');
+const gerimasDOM = document.querySelector('#gerimas');
+const orderDOM = document.querySelector('.order');
+const buttonDOM = document.querySelector('button');
+
 
 
 buttonDOM.addEventListener('click', (event) => {
-    event.preventDefault(); // neleidzia puslapiui persikrauti
-    spanDOM.innerText = inputDOM.value;
-    inputDOM.value = '';
-})
+    event.preventDefault();
+
+    if (item.checked) {
+        return "nori";
+    }
+    return "nenori";
+
+
+
+    orderDOM.innerText = `Uzsakovas vardu ${customerInputDOM.value} ${sriubaDOM.value} sriubos, ${patiekalasDOM.value} pagrindinio patiekalo, ${desertasDOM.value} deserto ir ${gerimasDOM.value} yra pasirinktas gerimas.`;
+
+
+});
+
