94a37d546c81f6fbc432d63faa0eb25cfc168af3
rainverted <MY_NAME@example.com>
2021-06-07 19:29:11 +0300

    Gerimo uzsakymas

--- js/main.js
+++ js/main.js
@@ -7,13 +7,12 @@ const orderDOM = document.querySelector('.order');
 const buttonDOM = document.querySelector('button');
 
 
-
 buttonDOM.addEventListener('click', (event) => {
     event.preventDefault();
 
-    const gerimasDOM = document.querySelectorAll('input[name="gerimas"]');
+    const gerimaiDOM = document.querySelectorAll('input[name="gerimas"]');
     let selectedValue;
-    for (const gerimas of gerimasDOM) {
+    for (const gerimas of gerimaiDOM) {
         if (gerimas.checked) {
             selectedValue = gerimas.value;
             break;
@@ -33,6 +32,11 @@ function checkedItems(item) {
     return "nenori";
 
 }
+/*
+function checkedItems(item) {
+    return item.checked ? "nori" : "nenori";
+}
+*/
 
 
 
