01e6b19a5b5d9be76f620f59fa1fa4e757bc1466
rainverted <MY_NAME@example.com>
2021-06-07 19:07:33 +0300

    Gerimo uzsakymas

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const customerInputDOM = document.querySelector('#customer');
 const sriubaDOM = document.querySelector('#sriuba');
 const patiekalasDOM = document.querySelector('#patiekalas');
 const desertasDOM = document.querySelector('#desertas');
-const gerimasDOM = document.querySelector('#gerimas');
+
 const orderDOM = document.querySelector('.order');
 const buttonDOM = document.querySelector('button');
 
@@ -11,15 +11,30 @@ const buttonDOM = document.querySelector('button');
 buttonDOM.addEventListener('click', (event) => {
     event.preventDefault();
 
+    const gerimasDOM = document.querySelectorAll('input[name="gerimas"]');
+    let selectedValue;
+    for (const gerimas of gerimasDOM) {
+        if (gerimas.checked) {
+            selectedValue = gerimas.value;
+            break;
+        }
+    }
+
+    orderDOM.innerText = `Uzsakovas vardu ${customerInputDOM.value} ${checkedItems(sriubaDOM)} sriubos, ${checkedItems(patiekalasDOM)} pagrindinio patiekalo, ${checkedItems(desertasDOM)} deserto ir ${selectedValue} yra pasirinktas gerimas.`;
+
+
+});
+
+function checkedItems(item) {
+
     if (item.checked) {
         return "nori";
     }
     return "nenori";
 
+}
 
 
-    orderDOM.innerText = `Uzsakovas vardu ${customerInputDOM.value} ${sriubaDOM.value} sriubos, ${patiekalasDOM.value} pagrindinio patiekalo, ${desertasDOM.value} deserto ir ${gerimasDOM.value} yra pasirinktas gerimas.`;
 
 
-});
 
