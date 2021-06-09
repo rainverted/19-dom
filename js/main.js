7064aca3db5e3a93a8e3c29d703790e56fe79f55
rainverted <MY_NAME@example.com>
2021-06-09 20:44:01 +0300

    socials

--- js/main.js
+++ js/main.js
@@ -1,17 +1,31 @@
 function generuotiSocials(selector, icons) {
     const iconsDOM = document.querySelector(selector);
-    let HTML = '';
-
-    for (let i = 0; i <= icons; i++) {
-        HTML += `<div>${icons}</div>`;
+    if (selector === '' ||
+        typeof selector !== 'string') {
+        console.error('ERROR: nerastas elementas');
+        return false;
     }
-    console.log(HTML);
 
+    if (!Array.isArray(icons) ||
+        icons.length === []) {
+        console.error('ERROR: negali buti tuscias array');
+        return false;
+    }
+
+    let HTML = '';
+    for (let i = 0; i < icons.length; i++) {
+        if (typeof icons[i] === 'string' &&
+            icons[i] !== '') {
+            HTML += `<i class="fa fa-${icons[i]}"></i>`;
+            console.log(icons);
+        }
+
+    }
 
     iconsDOM.innerHTML = HTML;
 }
 
-const icons = ['facebook', 'twitter', 'linkedin'];
+const icons = ['facebook', 'twitter', 5, [], '', 'linkedin'];
 
 generuotiSocials('.socials', icons);
 
@@ -33,4 +47,6 @@ generuotiSocials('.socials', icons);
     }
     DOM.innerHTML = HTML;
 }
-generuotiLenta('.lenta', 8);*/
+generuotiLenta('.lenta', 8);
+
+*/
