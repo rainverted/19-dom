540d8098964b74c28bac8d143fd8042458d45dfd
rainverted <MY_NAME@example.com>
2021-06-09 18:27:42 +0300

    socials uzdavinys

--- js/main.js
+++ js/main.js
@@ -1,62 +1,36 @@
-const lentaDOM = document.querySelector('.lenta');
-const namuRezultatasDOM = lentaDOM.querySelector('[data-komanda="namu"]');
-const sveciuRezultatasDOM = lentaDOM.querySelector('[data-komanda="sveciu"]');
+function generuotiSocials(selector, icons) {
+    const iconsDOM = document.querySelector(selector);
+    let HTML = '';
 
-const aiksteleDOM = document.querySelector('.aikstele');
-const namuKomandaDOM = aiksteleDOM.querySelector('[data-komanda="namu"]');
-const namuTaskaiDOM = namuKomandaDOM.querySelectorAll('button');
-
-const sveciuKomandaDOM = aiksteleDOM.querySelector('[data-komanda="sveciu"]');
-const sveciuTaskaiDOM = sveciuKomandaDOM.querySelectorAll('button');
-
-let namuKomandosTaskai = 0;
+    for (let i = 0; i <= icons; i++) {
+        HTML += `<div>${icons}</div>`;
+    }
+    console.log(HTML);
 
 
-for (let i = 0; i < namuTaskaiDOM.length; i++) {
-    const button = namuTaskaiDOM[i];
-
-    button.addEventListener('click', () => {
-        namuKomandosTaskai += i + 1;
-        namuRezultatasDOM.innerText = namuKomandosTaskai;
-    })
-
+    iconsDOM.innerHTML = HTML;
 }
 
-let sveciuKomandosTaskai = 0;
+const icons = ['facebook', 'twitter', 'linkedin'];
 
-for (let i = 0; i < sveciuTaskaiDOM.length; i++) {
-    const button = sveciuTaskaiDOM[i];
+generuotiSocials('.socials', icons);
 
-    button.addEventListener('click', () => {
-        sveciuKomandosTaskai += i + 1;
-        sveciuRezultatasDOM.innerText = sveciuKomandosTaskai;
-    })
 
+
+/*function generuotiLenta(selector, count) {
+    const DOM = document.querySelector(selector);
+    if (!DOM) {
+        console.error('ERROR: nerastas elementas');
+        return false;
+    }
+    if (count < 0 || count % 1 !== 0) {
+        console.error('ERROR: skaicius turi buti teigiamas ir sveikasis');
+        return false;
+    }
+    let HTML = '';
+    for (let i = 1; i <= count; i++) {
+        HTML += `<div>${i}</div>`;
+    }
+    DOM.innerHTML = HTML;
 }
-
-/*
-Alternative way when for is used for both arrays:
-
-let namuRezultatas = 0;
-let sveciuRezultatas = 0;
-
-for (let i = 0; i < 3; i++) {
-    namuTaskaiDOM[i].addEventListener('click', () => {
-        namuRezultatas += i + 1;
-        namuRezultatasDOM.innerText = namuRezultatas;
-    })
-
-    sveciuTaskaiDOM[i].addEventListener('click', () => {
-        sveciuRezultatas += i + 1;
-        sveciuRezultatasDOM.innerText = sveciuRezultatas;
-    })
-}
-
-
-*/
-
-
-
-
-
-
+generuotiLenta('.lenta', 8);*/
