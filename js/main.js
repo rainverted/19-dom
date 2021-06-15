5399c07d98e209f0a71a25504de5cc87c64476af
rainverted <MY_NAME@example.com>
2021-06-15 19:11:05 +0300

    nuo iki h parametrai

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
-function time() {
-    for (let h = 0; h < 24; h++) {
+function time(from, until) {
+    for (let h = from; h < until; h++) {
         for (let m = 0; m < 60; m += 15) {
             console.log(`${h}:${m}`);
         }
     }
 }
 
-time();
+time(1, 7);
+
+
+/*
+
+m = 0; 00
+m = 1; 15
+m = 2; 30
+
+*/
