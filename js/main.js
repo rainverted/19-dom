d1535309f2d256da71b5f7e125c1306e5e5e3c27
rainverted <MY_NAME@example.com>
2021-06-15 21:10:14 +0300

    minuciu intervalo spausdinimas

--- js/main.js
+++ js/main.js
@@ -1,10 +1,35 @@
-function time(from, until) {
-    for (let h = from; h < until; h++) {
-        for (let m = 0; m < 60; m += 15) {
-            console.log(`${h}:${m}`);
-        }
+function time(from, until, step) {
+    const pamokuKiekis = Math.floor((until - from) * 60 / step);
+    console.log(pamokuKiekis + 1);
+
+    for (let i = from * 60; i <= until * 60; i += step) {
+        const m = i % 60;
+        const h = (i - m) / 60;
+        //const h = Math.floor(i / 60);
+        //const m = i - (h * 60);
+
+
+        console.log(i, `${h}:${m < 10 ? '0' + m : m}`);
     }
+
 }
 
-time(1, 7);
+time(8, 13, 45);
+time(1, 4, 31);
 
+/*
+8:00
+8:45
+9:30
+10:15
+11:00
+11:45
+12:30
+
+*/
+
+/*
+i=150;
+h = Math.floor(i / 60) = 2
+
+*/
