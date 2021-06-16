c0d39190cc967e6083ca74a9320a1b3ca5069faf
rainverted <MY_NAME@example.com>
2021-06-16 18:49:23 +0300

    Laikrodis kai intervalas keiciasi vidurnakti

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
 function time(nuo, iki, trukme) {
-    for (let i = nuo * 60; i <= iki * 60; i += trukme) {
+    let pakeistasIki = iki;
+    if (nuo > iki) {
+        pakeistasIki = 24 + iki;
+    }
+    for (let i = nuo * 60; i <= pakeistasIki * 60; i += trukme) {
         const h = Math.floor(i / 60);
         const m = i - h * 60;
-        console.log(`${h}:${m < 10 ? '0' + m : m}`);
+        const hMOD = h % 24;
+        console.log(`${h < 10 ? '0' + hMOD : hMOD}:${m < 10 ? '0' + m : m}`);
     }
 }
 
-time(8, 11, 45);
+time(22, 1, 45);
+
+/*
+22:00
+22:45
+23:30
+0:15
+1:00
+*/
